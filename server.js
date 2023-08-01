0e55e0a9b0f268d3cd9cafa396ee69e2976317b3
Darren <hldaz77@gmail.com>
2023-08-01 12:49:01 +0100

    delete and put endpoints added

--- server.js
+++ server.js
@@ -23,11 +23,50 @@ app.get("/books", async (request, response) => {
 app.post("/books", async (request, response) => {
   try {
     const newBook = await Book.create(request.body);
-    response.status(200).json(newBook);
+    response
+      .status(201)
+      .json({ message: "New book successfully saved", book: newBook });
   } catch (error) {
     response
       .status(500)
       .json({ error: "Error creating book", details: error.message });
+  }
+});
+
+app.delete("/books/:id", async (request, response) => {
+  const bookId = request.params.id;
+
+  try {
+    const deletedBook = await Book.findByIdAndDelete(bookId);
+    if (!deletedBook) {
+      return response.status(404).json({ error: "Book not found" });
+    }
+
+    response.status(200).json({ message: "Book deleted successfully" });
+  } catch (error) {
+    response
+      .status(500)
+      .json({ error: "Error deleting book", details: error.message });
+  }
+});
+
+app.put("/books/:id", async (request, response) => {
+  const bookId = request.params.id;
+
+  try {
+    const updatedBook = await Book.findByIdAndUpdate(bookId, request.body, {
+      new: true,
+    });
+
+    if (!updatedBook) {
+      return response.status(404).json({ error: "Book not found" });
+    }
+
+    response.status(200).json(updatedBook);
+  } catch (error) {
+    response
+      .status(500)
+      .json({ error: "Error updating book", details: error.message });
   }
 });
 
